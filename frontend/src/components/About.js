d1fb831a4388ec569d7d5446a8d2f7bab909387e
Hritwik Tripathi <Tripathi.hritwik@gmail.com>
2021-11-14 20:23:48 +0530

    final push ig

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -5,7 +5,7 @@ import { Button,CardText } from "reactstrap";
 export default function About() {
   return (
     <>
-      <div alt="mission" style={{marginTop:"50px"}}>
+      <div alt="mission" style={{ marginTop: "50px" }}>
         <h1
           style={{
             fontSize: "30px",
@@ -16,10 +16,14 @@ export default function About() {
         >
           Our Mission Statement
         </h1>
-        <p style={{ fontSize: "20px", margin: "20px" }}>
-          Insurance.com educates customers and develops direct relationships
-          with carriers to help all customers shop real rates and get the right
-          insurance for their current needs.
+        <p style={{ fontSize: "20px", margin: "20px", color: "#346194" }}>
+          InstaSafe is a peer to peer insurance system that runs on top of
+          ethereum blockchain.The platform is designed to eliminate the middle
+          agent in the insurance system and shorten the verification time to
+          almost zero.Our system also enables users to stream their money at a
+          fixed rate thats way both the user and the platform are in control of
+          the money and nobody loses ,also the premium is dynamic according to
+          the location for fair treatment of the system
         </p>
       </div>
 
@@ -34,10 +38,19 @@ export default function About() {
         >
           Our Team
         </h1>
-        <p style={{ fontSize: "20px", margin: "20px" }}>
-          We’re proud to be a diverse team united by values including
-          openness, positivity, and drive that stems from our developers. No
-          matter how much our teams grow, we still feel like family.
+        <p style={{ fontSize: "20px", margin: "20px", color: "#346194" }}>
+          We’re proud to be a diverse team united by values including openness,
+          positivity, and drive that stems from our developers. No matter how
+          much our teams grow, we still feel like family.We understand that no
+          two ministries are the same, no two workers are identical, and we
+          strive to understand your unique needs and provide the best possible
+          benefits solutions. Our leadership team is comprised of caring,
+          educated and experienced professionals who face each decision with one
+          question in mind, “What is the best solution for those we serve?” At
+          every level of the organization you will find compassionate,
+          knowledgeable team members ready to meet your needs and answer your
+          questions. Taking care of your benefits isn’t just a job, it is our
+          passion to help those in His service.
         </p>
       </div>
       <div
@@ -67,12 +80,16 @@ export default function About() {
                 height: "200px",
                 width: "200px",
                 marginLeft: "25%",
+                marginBottom: "20px",
               }}
             />
-            <h2 style={{ marginLeft: "25%",color:"#ebebeb" }}>Hritwik Tripathi</h2>
-            <CardText style={{ marginLeft: "5%", marginRight: "5%",color:"#ebebeb" }}>
-              This card has supporting text below as a natural lead-in to
-              additional content.
+            <h2 style={{ marginLeft: "25%", color: "#ebebeb" }}>
+              Vineet Kumar
+            </h2>
+            <CardText
+              style={{ marginLeft: "5%", marginRight: "5%", color: "#ebebeb" }}
+            >
+              Hard choices, easy life. Easy choices, hard life.
             </CardText>
             <Button
               style={{
@@ -80,7 +97,7 @@ export default function About() {
                 marginLeft: "40%",
                 marginRight: "auto",
               }}
-              href="https://www.linkedin.com/in/hritwikt/"
+              href="https://www.linkedin.com/in/vineet-kumar-a67792197/"
             >
               <img
                 alt="linkedin_prop"
@@ -109,12 +126,16 @@ export default function About() {
                 height: "200px",
                 width: "200px",
                 marginLeft: "25%",
+                marginBottom: "20px",
               }}
             />
-            <h2 style={{ marginLeft: "25%",color:"#ebebeb" }}>Hritwik Tripathi</h2>
-            <CardText style={{ marginLeft: "5%", marginRight: "5%",color:"#ebebeb" }}>
-              This card has supporting text below as a natural lead-in to
-              additional content.
+            <h2 style={{ marginLeft: "25%", color: "#ebebeb" }}>
+              Hritwik Tripathi
+            </h2>
+            <CardText
+              style={{ marginLeft: "5%", marginRight: "5%", color: "#ebebeb" }}
+            >
+              Patience and hard work pay off, never doubt or lose faith in them.
             </CardText>
             <Button
               style={{
@@ -145,18 +166,22 @@ export default function About() {
           <div style={{ marginTop: "20px" }}>
             <img
               alt="CardImage"
-              src="https://siasky.net/OACgFb5Uek4vOw-_eR_z-BRvyT9rjqk3OPdV6nvTAT8erQ"
+              src="https://ipfs.io/ipfs/bafkreidrbxjca7o3x6dlcxica3b6e3qfkly5gyggzqburqsc3hnrespnou"
               style={{
                 borderRadius: "10px",
                 height: "200px",
                 width: "200px",
                 marginLeft: "25%",
+                marginBottom: "20px",
               }}
             />
-            <h2 style={{ marginLeft: "25%",color:"#ebebeb" }}>Hritwik Tripathi</h2>
-            <CardText style={{ marginLeft: "5%", marginRight: "5%",color:"#ebebeb" }}>
-              This card has supporting text below as a natural lead-in to
-              additional content.
+            <h2 style={{ marginLeft: "25%", color: "#ebebeb" }}>
+              Gautam Patodi
+            </h2>
+            <CardText
+              style={{ marginLeft: "5%", marginRight: "5%", color: "#ebebeb" }}
+            >
+              Hard work pays off if you’re patient enough to see it through.
             </CardText>
             <Button
               style={{
@@ -164,7 +189,7 @@ export default function About() {
                 marginLeft: "40%",
                 marginRight: "auto",
               }}
-              href="https://www.linkedin.com/in/hritwikt/"
+              href="https://www.linkedin.com/in/gautam-patodi-7a6849195/"
             >
               <img
                 alt="linkedin_prop"
@@ -183,7 +208,7 @@ export default function About() {
           alignItems: "center",
           background: "#ffffff",
           marginTop: "50px",
-          marginBottom:"100px"
+          marginBottom: "100px",
         }}
       >
         <div
@@ -198,18 +223,23 @@ export default function About() {
           <div style={{ marginTop: "20px" }}>
             <img
               alt="CardImage"
-              src="https://siasky.net/OACgFb5Uek4vOw-_eR_z-BRvyT9rjqk3OPdV6nvTAT8erQ"
+              src="https://ipfs.io/ipfs/bafkreidkwdurljvbinmvymf6ukdxm2y3udfjv5f7357k7lkqho5zxmfdfi"
               style={{
                 borderRadius: "10px",
                 height: "200px",
                 width: "200px",
                 marginLeft: "25%",
+                marginBottom: "20px",
               }}
             />
-            <h2 style={{ marginLeft: "25%",color:"#ebebeb" }}>Hritwik Tripathi</h2>
-            <CardText style={{ marginLeft: "5%", marginRight: "5%",color:"#ebebeb" }}>
-              This card has supporting text below as a natural lead-in to
-              additional content.
+            <h2 style={{ marginLeft: "25%", color: "#ebebeb" }}>
+              Mayank Verma
+            </h2>
+            <CardText
+              style={{ marginLeft: "5%", marginRight: "5%", color: "#ebebeb" }}
+            >
+              The harder you work for something, the greater you will feel when
+              you achieve it.
             </CardText>
             <Button
               style={{
@@ -217,7 +247,7 @@ export default function About() {
                 marginLeft: "40%",
                 marginRight: "auto",
               }}
-              href="https://www.linkedin.com/in/hritwikt/"
+              href="https://www.linkedin.com/in/mayank-verma-61444a18b/"
             >
               <img
                 alt="linkedin_prop"
@@ -246,12 +276,16 @@ export default function About() {
                 height: "200px",
                 width: "200px",
                 marginLeft: "25%",
+                marginBottom: "20px",
               }}
             />
-            <h2 style={{ marginLeft: "25%",color:"#ebebeb" }}>Hritwik Tripathi</h2>
-            <CardText style={{ marginLeft: "5%", marginRight: "5%",color:"#ebebeb" }}>
-              This card has supporting text below as a natural lead-in to
-              additional content.
+            <h2 style={{ marginLeft: "25%", color: "#ebebeb" }}>
+              Vaibhav Ekka
+            </h2>
+            <CardText
+              style={{ marginLeft: "5%", marginRight: "5%", color: "#ebebeb" }}
+            >
+              Stay dedicated it’s not going to happen overnight.
             </CardText>
             <Button
               style={{
@@ -259,7 +293,7 @@ export default function About() {
                 marginLeft: "40%",
                 marginRight: "auto",
               }}
-              href="https://www.linkedin.com/in/hritwikt/"
+              href="https://www.linkedin.com/in/vaibhav-ekka-26424118b/"
             >
               <img
                 alt="linkedin_prop"
@@ -271,10 +305,7 @@ export default function About() {
         </div>
       </div>
 
-
-
-      <p style={{marginLeft:"45%"}}>Made with ❤️ by Team Instasafe</p>   
- 
+      <p style={{ marginLeft: "45%" }}>Made with ❤️ by Team InstaSafe</p>
     </>
   );
 }
